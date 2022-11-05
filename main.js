c56713696a1ca082f75834bcb5038eaa65c282dd
Call333 <cam4@aluno.ifal.edu.br>
2022-11-05 09:36:57 -0300

    consertando o delay da animation

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ function createGame(player1, hours, player2) {
     `;
 }
 
-let delay = -0.4;
 
+let delay = -0.4;
 function createCard(date, day, games) {
   delay = delay + 0.4;
   return `
-        <div class="card" style="animation-delay: ${delay}">
+        <div class="card" style="animation-delay: ${delay}s;">
             <h2>${date} <span>${day}</span></h2>
                 <ul>
                     ${games}
